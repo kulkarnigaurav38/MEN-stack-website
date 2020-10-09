c4a10477ddf860d2b624c8d64a716e9f34391744
Gaurav Kulkarni <kulkarnigaurav38@gmail.com>
2020-10-09 21:46:20 +0000

    npm install mongodb and change the url part back to process.env.DATABASEURL

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 // Connecting to the database
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true}); 
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true}); 
  console.log(process.env.DATABASEURL);
 
 app.use(bodyParser.urlencoded({extended: true}));
