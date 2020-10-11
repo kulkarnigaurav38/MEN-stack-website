85aa4140ae8e5d5a33026f052828c233a6d327fc
Gaurav Kulkarni <kulkarnigaurav38@gmail.com>
2020-10-11 13:46:51 +0000

    commit node_modules directory to git as well

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -53,7 +53,7 @@ router.get("/:id", function(req, res){
 			res.redirect("back");
             console.log(err);
         } else {
-            console.log(foundCampground)
+            //console.log(foundCampground)
             //render show template with that campground
             res.render("campgrounds/show", {campground: foundCampground});
         }
